1eeb34db9dc62b27ced6e4b6b2483cb77ff712dc
fernando5030 <fernando.dealmeida@hotmail.com>
2017-10-07 19:18:33 -0300

    Close #1

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -51,15 +51,17 @@ class Troco {
     class TrocoIterator implements Iterator<PapelMoeda> {
 
         protected Troco troco;
+        protected int indice;
 
         public TrocoIterator(Troco troco) {
             this.troco = troco;
+            this.indice = 0;
         }
 
         @Override
         public boolean hasNext() {
-            for (int i = 6; i >= 0; i++) {
-                if (troco.papeisMoeda[i] != null) {
+            if(indice < papeisMoeda.length){
+                if (troco.papeisMoeda[indice] != null) {
                     return true;
                 }
             }
